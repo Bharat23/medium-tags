1238e951069e7f0079df54ead0caf3a9af5e201b
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-03-28 14:12:28 -0400

    add language detection in R

--- medium.R
+++ medium.R
@@ -1,21 +1,32 @@
 install.packages('rvest');
+install.packages('textcat');
 require('rvest');
+require('textcat');
 
 data = read.csv('./Medium_Clean.csv');
 
 contentList = vector();
- for (i in seq(1, nrow(data))) {
+ for (i in seq(1, 10)) {
    url = as.vector(data[i, 'url']);
    texts = tryCatch({
      webpage = read_html(url);
      pTags = html_nodes(webpage, 'p');
      pText = html_text(pTags);
-     pText;
+     language = textcat::textcat(pText);
+     if (language == 'english') {
+       print('here')
+        return(pText);
+     } else {
+       return('non-english');
+     }
    }, error = function(error_condition) {
+     print(error_condition)
      return('bad html')
    });
    contentList[i] = paste(texts, sep = " ", collapse = " ");
  }
 length(contentList)
- 
 
+
+textcat::textcat(contentList[4])
+contentList[5]
