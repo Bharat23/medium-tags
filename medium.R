abe999b64b8d0971953c1574248b23306e24081e
Bharat Sinha <bharat.sinha.2307@gmail.com>
2019-03-28 15:49:56 -0400

    add filter and csv write

--- medium.R
+++ medium.R
@@ -1,3 +1,8 @@
+
+Bharat Sinha
+3:48 PM (0 minutes ago)
+to me
+
 install.packages('rvest');
 install.packages('textcat');
 require('rvest');
@@ -5,30 +10,33 @@ require('textcat');
 
 data = read.csv('./Medium_Clean.csv', stringsAsFactors = FALSE);
 
-contentList = vector();
- for (i in seq(1, 10)) {
-   url = as.vector(data[i, 'url']);
-   texts = tryCatch({
-     webpage = read_html(url);
-     pTags = html_nodes(webpage, 'p');
-     pText = html_text(pTags);
-     language = textcat::textcat(pText);
-     if (language[1] == 'english') {
-        pText;
-     } else {
-       msg = 'non-english';
-       msg;
-     }
-   }, error = function(error_condition) {
-     return(error_condition)
-   });
-   contentList[i] = paste(texts, sep = " ", collapse = " ");
- }
-length(contentList)
+contentList = data.frame();
+for (i in seq(1, 100)) {
+  url = as.vector(data[i, 'url']);
+  contentList[i, 'url'] = url;
+  texts = tryCatch({
+    webpage = read_html(url);
+    pTags = html_nodes(webpage, 'p');
+    pText = html_text(pTags);
+    language = textcat::textcat(pText);
+    if (!is.na(language[1]) && language[1] == 'english') {
+      print('here')
+      pText;
+    } else {
+      msg = NA;
+      msg;
+    }
+  }, error = function(error_condition) {
+    print(error_condition)
+    msg = NA;
+    msg;
+  });
+  contentList[i, 'content'] = paste(texts, sep = " ", collapse = " ");
+}
 
 
-textcat::textcat(contentList[4])
-contentList[5]
+new_data=merge(data[1:100,], contentList, by.y = "url");
+new_data_filter_1 = filter(new_data, new_data['content'] != 'NA')
+new_data_filter_2 = filter(new_data, new_data['content'] != 'bad html')
 
-data[4,2]
-merge(data[,10], contentList)
+write.csv(new_data_filter_1, file = "MyData.csv")
